85b2f62dfd5d953ec99ff42577d0ad934782d5a3
felliks-c <feliksnekot@gmail.com>
2025-11-01 05:32:45 +0500

    imported one last forgoted service

--- services/__init__.py
+++ services/__init__.py
@@ -1,11 +1,12 @@
 from .create_vendor import create_vendor
-from .get_vendors import get_vendors
+from .get_vendors import get_vendors, search_vendors
 from .update_vendor import update_vendor
 from .delete_vendor import delete_vendor
 
 __all__ = [
     "create_vendor",
     "get_vendors",
+    "search_vendors",
     "update_vendor",
     "delete_vendor",
 ]
