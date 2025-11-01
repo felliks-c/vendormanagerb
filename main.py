810e5528cc0d95186842f07e905f07e992e11a98
felliks-c <feliksnekot@gmail.com>
2025-11-01 05:34:20 +0500

    fixed problems on main & removed critical functions

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, APIRouter
-from pydantic import BaseModel
+from fastapi import FastAPI
 import uvicorn
-import os
 from routers.vendors import router as vendors_router
 
 
@@ -16,21 +14,7 @@ async def read_root():
 async def read_root():
     return {"status": "success"}
 
-def load_private_key():
-    key_path = os.getenv("PRIVATE_KEY_PATH", "/run/secrets/vendormanagerb.pem")
-    with open(key_path, "r") as f:
-        private_key = f.read()
-    return private_key
-
-
-# Подключаем роутер к приложению
 app.include_router(vendors_router)
-
-
-# Пример как подключать роутеры из других модулей:
-# from .routes.vendors import router as vendors_router
-# app.include_router(vendors_router, prefix="/vendors", tags=["vendors"])
-
 
 if __name__ == "__main__":
     # Запуск для разработки
